874ae79e060545ee0b6fa480b3c04170e70445e7
Ananth Prabhu T <120672829+prabhuanantht@users.noreply.github.com>
2026-02-25 22:03:00 +0530

    Updates

--- src/data/links.ts
+++ src/data/links.ts
@@ -6,6 +6,6 @@ export const Links = {
     resume: "https://drive.google.com/file/d/1fR8yCBKKkCo29OH0uUh59gsKw4gIBGUd/view?usp=sharing",
     emailonly: "prabhuanantht@gmail.com",
     location: "Mysuru, India",
-    phone: "+91 8105385164",
+    phone: "+91XXXXXXXXXX",
     chess: "https://www.chess.com/member/frozenfire051",
   };
